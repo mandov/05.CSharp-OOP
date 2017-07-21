a9f5d8e442e3b2fc7d965531020f11c713d0103b
mandov <gabi_nm@abv.bg>
2017-07-21 21:41:37 +0300

    Pushing OOP-Principlese-Part 2 Homework

---
+++ 05.OOP- Principles - Part 2/01.Shapes/DemoShapes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Shapes;
+
+class DemoShapes
+{
+    static void Main()
+    {
+        Shape[] shapes = new Shape[]
+        {
+            new Rectangle(2,5),
+            new Triangle(4, 3),
+            new Square(5)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine("{0} Surface:{1}",shape.GetType().Name,shape.CalculateSurface());
+        }
+    }
+}
---
+++ 05.OOP- Principles - Part 2/02.BankAccounts/DepositAccount.cs
@@ -0,0 +1,41 @@
+﻿namespace BankAccounts
+{
+    using System;
+    using BankAccounts.Interfaces;
+
+    public class DepositAccount : Account, IWithdrawable
+    {
+        public DepositAccount(Customer customer, decimal balance, decimal interestRate)
+        : base(customer, balance, interestRate)
+        {
+        }
+
+        public override decimal InterestAmount(int months)
+        {
+            if (this.Balance < 1000)
+            {
+                Console.WriteLine("There is no Interest amount if your balance is under 1000 !");
+                return this.InterestRate;
+            }
+            return months * InterestRate;
+        }
+
+        public void WithDraw(decimal amount)
+        {
+
+            while (amount <= 0)
+            {
+                Console.WriteLine("You need to enter number that is greater than zero !");
+                amount = decimal.Parse(Console.ReadLine());
+            }
+
+            while (this.Balance < amount)
+            {
+                Console.WriteLine("You don't have enough balance, please enter new mooney amount");
+                amount = decimal.Parse(Console.ReadLine());
+            }
+
+            this.Balance -= amount;
+        }
+    }
+}
